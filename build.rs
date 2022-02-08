760857372f728f9dfdeecc7f0357fbd148feb5c9
Richard <huachao.huang@gmail.com>
2022-02-08 22:45:58 +0800

    Merge Databases and Collections into Universe

--- build.rs
+++ build.rs
@@ -1,11 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().compile(
-        &[
-            "proto/txn.proto",
-            "proto/databases.proto",
-            "proto/collections.proto",
-        ],
-        &["proto"],
-    )?;
+    tonic_build::configure().compile(&["proto/txn.proto", "proto/universe.proto"], &["proto"])?;
     Ok(())
 }
