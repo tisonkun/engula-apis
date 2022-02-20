f2a3ed2fd328f7e2b94075744a6b38c793334c93
Richard <huachao.huang@gmail.com>
2022-02-20 11:53:03 +0800

    Update object messages

--- src/lib.rs
+++ src/lib.rs
@@ -18,101 +18,106 @@ tonic::include_proto!("engula.v1");
 
 use std::collections::HashMap;
 
-impl Value {
-    pub fn as_i64(self) -> Option<i64> {
-        self.value.and_then(|v| {
-            if let value::Value::Int64Value(v) = v {
-                Some(v)
-            } else {
-                None
-            }
-        })
+pub type Value = value_union::Value;
+
+impl From<Value> for ValueUnion {
+    fn from(v: Value) -> Self {
+        Self { value: Some(v) }
     }
 }
 
-impl From<&[u8]> for Value {
-    fn from(v: &[u8]) -> Self {
-        Self {
-            value: Some(value::Value::BlobValue(v.to_owned())),
-        }
+impl From<Option<Value>> for ValueUnion {
+    fn from(v: Option<Value>) -> Self {
+        Self { value: v }
     }
 }
 
-impl From<Vec<u8>> for Value {
-    fn from(v: Vec<u8>) -> Self {
-        Self {
-            value: Some(value::Value::BlobValue(v)),
-        }
-    }
-}
-
-impl From<&str> for Value {
-    fn from(v: &str) -> Self {
-        Self {
-            value: Some(value::Value::TextValue(v.to_owned())),
-        }
-    }
-}
-
-impl From<String> for Value {
-    fn from(v: String) -> Self {
-        Self {
-            value: Some(value::Value::TextValue(v)),
-        }
-    }
-}
-
-impl From<i32> for Value {
-    fn from(v: i32) -> Self {
-        Self {
-            value: Some(value::Value::Int64Value(v.into())),
-        }
+impl From<ValueUnion> for Option<Value> {
+    fn from(v: ValueUnion) -> Self {
+        v.value
     }
 }
 
 impl From<i64> for Value {
     fn from(v: i64) -> Self {
-        Self {
-            value: Some(value::Value::Int64Value(v)),
+        Self::I64Value(v)
+    }
+}
+
+impl TryFrom<Value> for i64 {
+    type Error = Value;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        if let Value::I64Value(v) = v {
+            Ok(v)
+        } else {
+            Err(v)
         }
     }
 }
 
-impl From<SequenceValue> for Value {
-    fn from(v: SequenceValue) -> Self {
-        Self {
-            value: Some(value::Value::SequenceValue(v)),
-        }
+impl From<&[u8]> for Value {
+    fn from(v: &[u8]) -> Self {
+        Self::BlobValue(v.to_owned())
     }
 }
 
-impl<T: Into<Value>> From<Vec<T>> for Value {
-    fn from(values: Vec<T>) -> Self {
-        SequenceValue {
-            values: values.into_iter().map(|v| v.into()).collect(),
-        }
-        .into()
+impl From<Vec<u8>> for Value {
+    fn from(v: Vec<u8>) -> Self {
+        Self::BlobValue(v)
     }
 }
 
-impl From<AssociativeValue> for Value {
-    fn from(v: AssociativeValue) -> Self {
-        Self {
-            value: Some(value::Value::AssociativeValue(v)),
-        }
+impl From<&str> for Value {
+    fn from(v: &str) -> Self {
+        Self::TextValue(v.to_owned())
     }
 }
 
-impl<T: Into<Value>> From<HashMap<Vec<u8>, T>> for Value {
-    fn from(map: HashMap<Vec<u8>, T>) -> Self {
+impl From<String> for Value {
+    fn from(v: String) -> Self {
+        Self::TextValue(v)
+    }
+}
+
+impl From<MappingValue> for Value {
+    fn from(v: MappingValue) -> Self {
+        Value::MappingValue(v)
+    }
+}
+
+impl<K, V> From<HashMap<K, V>> for Value
+where
+    K: Into<Value> + Ord,
+    V: Into<Value>,
+{
+    fn from(map: HashMap<K, V>) -> Self {
         let (keys, values) = map.into_iter().fold(
             (Vec::new(), Vec::new()),
             |(mut keys, mut values), (k, v)| {
-                keys.push(k);
-                values.push(v.into());
+                keys.push(k.into().into());
+                values.push(v.into().into());
                 (keys, values)
             },
         );
-        AssociativeValue { keys, values }.into()
+        MappingValue { keys, values }.into()
+    }
+}
+
+impl From<RepeatedValue> for Value {
+    fn from(v: RepeatedValue) -> Self {
+        Self::RepeatedValue(v)
+    }
+}
+
+impl<T> From<Vec<T>> for Value
+where
+    T: Into<Value>,
+{
+    fn from(values: Vec<T>) -> Self {
+        RepeatedValue {
+            values: values.into_iter().map(|v| v.into().into()).collect(),
+        }
+        .into()
     }
 }
